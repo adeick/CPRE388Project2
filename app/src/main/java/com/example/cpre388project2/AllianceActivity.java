53ab0bd0b60e53850a3112add17d0080ff770e1e
Andrew Deick <37886715+adeick@users.noreply.github.com>
2022-12-01 17:33:49 -0600

    Added Alliances page

---
+++ app/src/main/java/com/example/cpre388project2/AllianceActivity.java
@@ -0,0 +1,14 @@
+package com.example.cpre388project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class AllianceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.alliance_search);
+    }
+}
--- app/src/main/java/com/example/cpre388project2/MainActivity.java
+++ app/src/main/java/com/example/cpre388project2/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.cpre388project2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,7 +21,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private BitcoinStorageModel bitcoinStorageModel;
+    private TextView bitcoinCountTextView;
     private TowerModel towerModel;
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,36 @@ public class MainActivity extends AppCompatActivity {
 
         bitcoinStorageModel.initialize();
 
-        TextView bitcoinCountTextView = findViewById(R.id.bitcoinCountTextView);
+        bitcoinCountTextView = findViewById(R.id.bitcoinCountTextView);
 
         bitcoinStorageModel.getAmountStored().observe(this, amount -> {
             bitcoinCountTextView.setText(getString(R.string.bitcoinCountText, bitcoinStorageModel.getAmountStored().getValue(), "Bit"));
         });
+
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener{
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG, "onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY){
+            Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+            return true;
+        }
     }
 
     public void MainframeOnClick(View view) {
