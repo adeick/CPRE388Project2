29977c255c1fd8a872317f132cc4826907cd51a5
David Harmon <dharmon1@iastate.edu>
2022-12-01 19:04:43 -0600

    Update build.gradle

--- app/src/main/java/com/example/cpre388project2/MainActivity.java
+++ app/src/main/java/com/example/cpre388project2/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
             bitcoinCountTextView.setText(getString(R.string.bitcoinCountText, bitcoinStorageModel.getAmountStored().getValue(), "Bit"));
         });
 
-    }
         startAutoClickers();
     }
 
