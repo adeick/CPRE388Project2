309bbd33949dd191326e3b78142259346fca7f4c
Fergen <bkfergen@iastate.edu>
2022-12-06 15:55:37 -0600

    shows members in an alliance

--- app/src/main/java/com/example/cpre388project2/AllianceActivity.java
+++ app/src/main/java/com/example/cpre388project2/AllianceActivity.java
@@ -1,5 +1,6 @@
 package com.example.cpre388project2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.cpre388project2.towers.Tower;
+import com.example.cpre388project2.towers.TowerTypes;
 import com.example.cpre388project2.util.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 
 public class AllianceActivity extends AppCompatActivity {
@@ -34,7 +46,8 @@ public class AllianceActivity extends AppCompatActivity {
 //            }
 //        });
     }
-    private void goToMain(){
+
+    private void goToMain() {
         Intent switchActivityIntent = new Intent(this, MainActivity.class);
         startActivity(switchActivityIntent);
     }
@@ -43,13 +56,36 @@ public class AllianceActivity extends AppCompatActivity {
         goToMain();
     }
 
-    public void onSoftwareEngineerClick(View view){
+    public void getUserList(String alliance) {
+        mFirestore.collection("users")
+                .whereEqualTo("alliance", alliance)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        TextView participant = findViewById(R.id.allianceMember1);
+                        if (task.isSuccessful()) {
+                            if (task.getResult().size() > 0) { // Members in alliance
+                                String text = "";
+                                for (DocumentSnapshot userInfo : task.getResult().getDocuments()) {
+                                    text += userInfo.get("username") + " - " + userInfo.getLong("bitcoins") + "\n";
+                                }
+
+                                participant.setText(text);
+                                return;
+                            }
+                        }
+                        participant.setText("No members");
+                    }
+                });
+    }
+
+    public void onSoftwareEngineerClick(View view) {
         setContentView(R.layout.alliance_join);
         allianceName = findViewById(R.id.allianceName);
         allianceName.setText("Software Engineers");
 
-        TextView participant = findViewById(R.id.allianceMember1);
-        participant.setText(FirebaseUtil.getAuth().getCurrentUser().getDisplayName());
+        getUserList("se");
 //        FireBaseUser thisUser = FirebaseUtil.getAuth().getCurrentUser().getUid();
 //
 //        mFirestore.collection("se").add({
@@ -57,15 +93,16 @@ public class AllianceActivity extends AppCompatActivity {
 //        });
     }
 
-    public void onBackButtonClicked(View view){
+    public void onBackButtonClicked(View view) {
         setContentView(R.layout.alliance_search);
     }
 
-    public void onComputerEngineerClick(View view){
+    public void onComputerEngineerClick(View view) {
         setContentView(R.layout.alliance_join);
         allianceName = findViewById(R.id.allianceName);
         allianceName.setText("Computer Engineers");
 
+        getUserList("cpre");
 //        FireBaseUser thisUser = FirebaseUtil.getAuth().getCurrentUser().getUid();
 //
 //        mFirestore.collection("se").add({
@@ -73,11 +110,12 @@ public class AllianceActivity extends AppCompatActivity {
 //        });
     }
 
-    public void onElectricalEngineerClick(View view){
+    public void onElectricalEngineerClick(View view) {
         setContentView(R.layout.alliance_join);
         allianceName = findViewById(R.id.allianceName);
         allianceName.setText("Electrical Engineers");
 
+        getUserList("ee");
 //        FireBaseUser thisUser = FirebaseUtil.getAuth().getCurrentUser().getUid();
 //
 //        mFirestore.collection("se").add({
@@ -85,11 +123,12 @@ public class AllianceActivity extends AppCompatActivity {
 //        });
     }
 
-    public void onCSClick(View view){
+    public void onCSClick(View view) {
         setContentView(R.layout.alliance_join);
         allianceName = findViewById(R.id.allianceName);
         allianceName.setText("CS Majors (lol)");
 
+        getUserList("cs");
 //        FireBaseUser thisUser = FirebaseUtil.getAuth().getCurrentUser().getUid();
 //
 //        mFirestore.collection("se").add({
